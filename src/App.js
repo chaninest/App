45db4ed9277f2764b8f4c5cea038186b56e67c03
chaninest <chaninest@MacBook-Air-khxng-chaninest.local>
2024-09-02 11:01:53 +0700

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import Transaction from "./component/Transaction";
+import './App.css'
+import FormComponent from "./component/formComponent";
+import { useState } from "react";
+import DataContext from "./data/DataContext";
+import ReportComponent from "./component/ReportComponent";
+import { useEffect } from "react";
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const design = {color:"orange",textAlign:"center",fontSize:"2rem"}
+
+const initData = [
+  {id:1,title:"ค่าเช่าบ้าน",amount:-3000},
+  {id:2,title:"เงินเดือน",amount:30000}
+]
+const [items,setItems] = useState(initData)
+
+const [reportIncome,setReportIncome]= useState(0)
+const [reportExpense,setReportExpense] = useState (0)
+
+const onAddNewItem = (newItem)=>{ 
+  setItems((prevItem)=>{
+    return [newItem,...prevItem]
+  })
+}
+useEffect(()=>{
+    const amounts = items.map(items=>items.amount)
+    const income = amounts.filter(element=>element>0).reduce((total,element)=>total+=element,0)
+    const expense = (amounts.filter(element=>element<0).reduce((total,element)=>total+=element,0))*-1
+    setReportIncome(income)
+    setReportExpense(expense)
+},[items,reportExpense,reportIncome])
+  return ( 
+   <DataContext.Provider value={{income : reportIncome,expense : reportExpense}}>
+    <div className="container">
+       <h1 style={design}>แอพบัญชีรายรับ - รายจ่าย</h1>
+      <Router>
+      <div>
+          <ul className="horizontal-menu">
+            <li>
+              <Link to="/">ข้อมูลบัญชี</Link>
+            </li>
+            <li>
+              <Link to="/insert">บันทึกข้อมูล</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/" exact >
+                <ReportComponent/>
+            </Route>
+            <Route path="/insert">
+            <FormComponent onAddItem={onAddNewItem}/>
+              <Transaction items={items}/>
+            </Route>
+          </Switch>
+       </div>
+      </Router>
     </div>
-  );
+
+  </DataContext.Provider>
+);
 }
 
 export default App;
